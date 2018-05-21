245b3e5dd8ce1fed15020a5b7891507a427d1a94
Kailin Cannon <kailin.cannon@gmail.com>
2018-05-21 09:37:43 -0600

    Form added

--- server.js
+++ server.js
@@ -12,6 +12,13 @@ app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Photo Trapper Keeper';
 
 
+
+
+
+
+
+
+
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
 });
